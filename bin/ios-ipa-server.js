70b96ccfce3f5d491fe5a77c6c573db595b7078a
bumaociyuan <zx19880528@gmail.com>
2016-06-12 15:36:24 +0800

    重构代码,无需手动生成证书

---
+++ bin/ios-ipa-server.js
@@ -0,0 +1,186 @@
+#!/usr/bin/env node
+
+var mkdirp = require('mkdirp');
+var program = require('commander');
+var fs = require('fs');
+var https = require('https');
+var path = require('path');
+var exit = process.exit;
+var pkg = require('../package.json');
+var version = pkg.version;
+
+var express = require('express');
+var mustache = require('mustache');
+var strftime = require('strftime');
+var underscore = require('underscore');
+var os = require('os');
+require('shelljs/global');
+
+
+var port = 1234;
+var ipAddress = underscore
+  .chain(require('os').networkInterfaces())
+  .values()
+  .flatten()
+  .find(function(iface) {
+    return iface.family === 'IPv4' && iface.internal === false;
+  })
+  .value()
+  .address;
+
+
+var globalCerFolder = os.homedir() + '/.ios-ipa-server/' + ipAddress;
+/**
+ * Main program.
+ */
+process.exit = exit
+
+// CLI
+
+before(program, 'outputHelp', function() {
+  this.allowUnknownOption();
+});
+
+program
+  .version(version)
+  .usage('[dir]')
+  .parse(process.argv);
+
+if (!exit.exited) {
+  main();
+}
+
+/**
+ * Install a before function; AOP.
+ */
+
+function before(obj, method, fn) {
+  var old = obj[method];
+
+  obj[method] = function() {
+    fn.call(this);
+    old.apply(this, arguments);
+  };
+}
+
+function main() {
+
+  console.log('https://' + ipAddress + ':' + port + '/download');
+  var destinationPath = program.args.shift() || '.';
+
+  var ipasDir = destinationPath;
+
+
+  var key;
+  var cert;
+
+  // try {
+  //   key = fs.readFileSync(globalCerFolder + '/mycert1.key', 'utf8');
+  //   cert = fs.readFileSync(globalCerFolder + '/mycert1.cer', 'utf8');
+  // } catch (e) {
+  //   exec('sh  ' + path.join(__dirname, '..', 'generate-certificate.sh'),
+  //     function(error, stdout, stderr) {
+  //       key = fs.readFileSync(globalCerFolder + '/mycert1.key', 'utf8');
+  //       cert = fs.readFileSync(globalCerFolder + '/mycert1.cer', 'utf8');
+  //     }
+  //   );
+  // }
+  try {
+    key = fs.readFileSync(globalCerFolder + '/mycert1.key', 'utf8');
+    cert = fs.readFileSync(globalCerFolder + '/mycert1.cer', 'utf8');
+  } catch (e) {
+    var result = exec('sh  ' + path.join(__dirname, '..', 'generate-certificate.sh')).output;
+    console.log(result);
+    key = fs.readFileSync(globalCerFolder + '/mycert1.key', 'utf8');
+    cert = fs.readFileSync(globalCerFolder + '/mycert1.cer', 'utf8');
+  }
+
+  var options = {
+    key: key,
+    cert: cert
+  };
+
+
+  var app = express();
+  app.use('/', express.static(__dirname + '/' + ipasDir));
+  app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+  app.use('/cer', express.static(globalCerFolder));
+
+  app.get(['/', '/download'], function(req, res, next) {
+
+    fs.readFile(path.join(__dirname, '..', 'templates') + '/download.html', function(err, data) {
+      if (err) throw err;
+      var template = data.toString();
+
+      var ipas = ipasInLocation(ipasDir);
+
+      var items = [];
+      for (var i = ipas.length - 1; i >= 0; i--) {
+        items.push(itemWithEnv(ipas[i], ipasDir));
+      };
+
+      items = items.sort(function(a, b) {
+        var result = b.time.getTime() - a.time.getTime();
+        // if (result > 0) {result = 1} else if (result < 0) { result = -1 };
+
+        return result;
+      });
+
+      var info = {};
+      info.ip = ipAddress;
+      info.port = port;
+      info.items = items;
+      var rendered = mustache.render(template, info);
+      res.send(rendered);
+    })
+  });
+
+
+  app.get('/plist/:file', function(req, res) {
+    fs.readFile(path.join(__dirname, '..', 'templates') + '/template.plist', function(err, data) {
+      if (err) throw err;
+      var template = data.toString();
+
+      var rendered = mustache.render(template, {
+        name: req.params.file,
+        ip: ipAddress,
+        port: port,
+      });
+
+      res.set('Content-Type', 'text/plain; charset=utf-8');
+      // res.set('MIME-Type', 'application/octet-stream');
+      res.send(rendered);
+    })
+  });
+
+  https.createServer(options, app).listen(port);
+
+}
+
+function itemWithEnv(env, ipasDir) {
+  var stat = fs.statSync(ipasDir + '/' + env + '.ipa');
+  var time = new Date(stat.mtime);
+  var timeString = strftime('%F %H:%M', time);
+  return {
+    name: env,
+    description: '   更新: ' + timeString,
+    time: time,
+    ip: ipAddress,
+    port: port,
+  }
+}
+
+/**
+ *
+ */
+
+function ipasInLocation(location) {
+  var result = [];
+  var files = fs.readdirSync(location);
+  for (var i in files) {
+    if (path.extname(files[i]) === ".ipa") {
+      result.push(path.basename(files[i], '.ipa'));
+    }
+  }
+  return result;
+}
--- generate-certificate.sh
+++ generate-certificate.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 
-mkdir cer
-cd cer
-
 ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+cerDir="$HOME/.ios-ipa-server/"$ip"/"
 echo $ip
+echo $cerDir
+mkdir -p "$cerDir"
 
-openssl genrsa -out myCA.key 2048
+openssl genrsa -out "$cerDir"myCA.key 2048
 
-openssl req -x509 -new -key myCA.key -out myCA.cer -days 730 -subj /CN="ZX Custom CA"
+openssl req -x509 -new -key "$cerDir"myCA.key -out "$cerDir"myCA.cer -days 730 -subj /CN="ios-ipa-server Custom CA"
 
-openssl genrsa -out mycert1.key 2048
+openssl genrsa -out "$cerDir"mycert1.key 2048
 
-openssl req -new -out mycert1.req -key mycert1.key -subj /CN=$ip
+openssl req -new -out "$cerDir"mycert1.req -key "$cerDir"mycert1.key -subj /CN=$ip
 
-openssl x509 -req -in mycert1.req -out mycert1.cer -CAkey myCA.key -CA myCA.cer -days 365 -CAcreateserial -CAserial serial
+openssl x509 -req -in "$cerDir"mycert1.req -out "$cerDir"mycert1.cer -CAkey "$cerDir"myCA.key -CA "$cerDir"myCA.cer -days 365 -CAcreateserial -CAserial serial
