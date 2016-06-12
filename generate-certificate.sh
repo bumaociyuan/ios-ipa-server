c7a92eda50e1292b4d023c164c09f36ab135dd1f
bumaociyuan <zx19880528@gmail.com>
2016-06-12 23:05:23 +0800

    remove generate-certificate.sh log

--- generate-certificate.sh
+++ generate-certificate.sh
@@ -2,16 +2,19 @@
 
 ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 cerDir="$HOME/.ios-ipa-server/"$ip"/"
-echo $ip
-echo $cerDir
+# echo $ip
+# echo $cerDir
 mkdir -p "$cerDir"
 
-openssl genrsa -out "$cerDir"myCA.key 2048
+# get rid of output
+blackhole="/dev/null"
 
-openssl req -x509 -new -key "$cerDir"myCA.key -out "$cerDir"myCA.cer -days 730 -subj /CN="ios-ipa-server Custom CA"
+openssl genrsa -out "$cerDir"myCA.key 2048 2> $blackhole
 
-openssl genrsa -out "$cerDir"mycert1.key 2048
+openssl req -x509 -new -key "$cerDir"myCA.key -out "$cerDir"myCA.cer -days 730 -subj /CN="ios-ipa-server Custom CA" 2> $blackhole
 
-openssl req -new -out "$cerDir"mycert1.req -key "$cerDir"mycert1.key -subj /CN=$ip
+openssl genrsa -out "$cerDir"mycert1.key 2048 2> $blackhole
 
-openssl x509 -req -in "$cerDir"mycert1.req -out "$cerDir"mycert1.cer -CAkey "$cerDir"myCA.key -CA "$cerDir"myCA.cer -days 365 -CAcreateserial -CAserial "$cerDir"serial
+openssl req -new -out "$cerDir"mycert1.req -key "$cerDir"mycert1.key -subj /CN=$ip 2> $blackhole
+
+openssl x509 -req -in "$cerDir"mycert1.req -out "$cerDir"mycert1.cer -CAkey "$cerDir"myCA.key -CA "$cerDir"myCA.cer -days 365 -CAcreateserial -CAserial "$cerDir"serial 2> $blackhole
